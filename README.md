24789d66645cf72997f10e136a94e5b21f3e0e6f
Edward Arcuri <earcuri@paloaltonetworks.com>
2018-06-25 21:55:02 -0600

    fix so containers start with system

--- README.md
+++ README.md
@@ -12,7 +12,11 @@ For the latest information and release specific notes view the [release notes](d
 To install, follow the directions below.  There is also a VM that can be obtained from your Palo Alto Networks account team that can be used for a proof-of-concept of SafeNetworking and comes pre-installed and ready to go.  
 
 #### NOTE: If you already have an ElasticStack cluster (i.e. ElasticCloud or a local install) skip to step 2
-1.) [Infrastructure Setup Instructions](https://github.com/PaloAltoNetworks/safe-networking/wiki/Infrastructure-Setup)
+There are documented ways to install the infrastructure.  Included is a docker-compose setup that installs and configures the ELK stack for you.  Optionally, you can install the ELK components "locally" and use those.  Docker is much easier.<br/><br/>
+
+1.) Choose one of the following:<br/>
+[Infrastructure Setup Instructions - DOCKER](https://github.com/PaloAltoNetworks/safe-networking/wiki/Infrastructure-Setup---DOCKER) - OR - [Infrastructure Setup Instructions - LOCAL](https://github.com/PaloAltoNetworks/safe-networking/wiki/Infrastructure-Setup)
+<br/>
 
 2.) [Install SafeNetworking](https://github.com/PaloAltoNetworks/safe-networking/wiki/Installing-the-SafeNetworking-Software)
 
--- install/setup.sh
+++ install/setup.sh
@@ -2,39 +2,39 @@
 # NOTE: If you are using something other than the default install, you may need
 # change the localhost settings below to your IP address
 echo "Installing index mappings into ElasticSearch"
-curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/af-details/' -d @elasticsearch/af-details.json
-curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/_template/traffic?pretty' -d @elasticsearch/traffic_template_mapping.json
-curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/_template/threat?pretty' -d @elasticsearch/threat_template_mapping.json
-curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/sfn-domain-details/' -d @elasticsearch/sfn-domain-details.json
-curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/sfn-tag-details/' -d @elasticsearch/sfn-tag-details.json
+curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/af-details/' -d @../infra/elasticsearch/mappings/af-details.json
+curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/_template/traffic?pretty' -d @../infra/elasticsearch/mappings/traffic_template_mapping.json
+curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/_template/threat?pretty' -d @../infra/elasticsearch/mappings/threat_template_mapping.json
+curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/sfn-domain-details/' -d @../infra/elasticsearch/mappings/sfn-domain-details.json
+curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/sfn-tag-details/' -d @../infra/elasticsearch/mappings/sfn-tag-details.json
 curl -XPUT -H'Content-Type: application/json' 'localhost:9200/_settings' -d '{"index" : {"number_of_replicas" : 0}}'
 
-echo ""
+echo "\n"
 
 # This sets up the automatic startup of SFN if the system reboots.  It also gives
 # the ability to control SafeNetworking as a service using the systemctl and
 # service commands
-INSTALL_DIR="$(dirname `pwd`)"
-START_FILE=$INSTALL_DIR/install/sfn/sfn.sh
-echo "Creating SafeNetworking startup file $START_FILE"
-echo "...."
-echo "#!/bin/sh -" >$START_FILE
-echo "# SafeNetworking startup file" >>$START_FILE
-echo "cd $INSTALL_DIR" >>$START_FILE
-echo "$INSTALL_DIR/env/bin/python $INSTALL_DIR/sfn" >>$START_FILE
-echo ""
-echo "Moving startup file to /usr/local/bin -->"
-echo "...."
-sudo cp $START_FILE /usr/local/bin/sfn.sh
-sudo chmod 755 /usr/local/bin/sfn.sh
-echo ""
-echo "Copying sfn.service to /etc/systemd/system -->"
-echo "...."
-sudo cp $INSTALL_DIR/install/sfn/sfn.service /etc/systemd/system/sfn.service
-echo ""
-sudo chmod 644 /etc/systemd/system/sfn.service
-echo "Enabling and starting the SafeNetworking Service - this may take a minute"
-sudo systemctl daemon-reload
-sudo systemctl enable sfn.service
-sudo systemctl start sfn.service
-sudo systemctl status sfn.service
+# INSTALL_DIR="$(dirname `pwd`)"
+# START_FILE=$INSTALL_DIR/install/sfn/sfn.sh
+# echo "Creating SafeNetworking startup file $START_FILE"
+# echo "...."
+# echo "#!/bin/sh -" >$START_FILE
+# echo "# SafeNetworking startup file" >>$START_FILE
+# echo "cd $INSTALL_DIR" >>$START_FILE
+# echo "$INSTALL_DIR/env/bin/python $INSTALL_DIR/sfn" >>$START_FILE
+# echo ""
+# echo "Moving startup file to /usr/local/bin -->"
+# echo "...."
+# sudo cp $START_FILE /usr/local/bin/sfn.sh
+# sudo chmod 755 /usr/local/bin/sfn.sh
+# echo ""
+# echo "Copying sfn.service to /etc/systemd/system -->"
+# echo "...."
+# sudo cp $INSTALL_DIR/install/sfn/sfn.service /etc/systemd/system/sfn.service
+# echo ""
+# sudo chmod 644 /etc/systemd/system/sfn.service
+# echo "Enabling and starting the SafeNetworking Service - this may take a minute"
+# sudo systemctl daemon-reload
+# sudo systemctl enable sfn.service
+# sudo systemctl start sfn.service
+# sudo systemctl status sfn.service
