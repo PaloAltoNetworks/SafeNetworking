5db53536abf0d32b52cdbab7a134bbdb28a69ebd
Nathan <nembery@gmail.com>
2018-07-25 15:32:19 -0400

    add try/except block to updateAfStats

--- project/dns/dnsutils.py
+++ project/dns/dnsutils.py
@@ -1,12 +1,13 @@
+import datetime
 import json
 import time
-import requests
-import datetime
-from project import app
 from ast import literal_eval
+
+import requests
 from elasticsearch.exceptions import NotFoundError
+
+from project import app
 from project.dns.dns import AFDetailsDoc, TagDetailsDoc, DomainDetailsDoc
-from elasticsearch_dsl import DocType, Search, Date, Integer, Keyword, Text, Ip, connections
 
 
 def updateAfStats():
@@ -23,28 +24,33 @@ def updateAfStats():
         app.logger.info(f"The af-details doc is not found - creating")
 
         afDoc = AFDetailsDoc(meta={'id': 'af-details'},
-                                    minute_points=0,
-                                    minute_points_remaining=0,
-                                    daily_points=0,
-                                    daily_points_remaining=0,
-                                    minute_bucket_start=now,
-                                    daily_bucket_start=now)
+                             minute_points=0,
+                             minute_points_remaining=0,
+                             daily_points=0,
+                             daily_points_remaining=0,
+                             minute_bucket_start=now,
+                             daily_bucket_start=now)
     # The AF Doc should exist by now
 
     # Just grab a tag that we know exists so we can get the rolling point total
     returnData = getTagInfo("WildFireTest")
     afInfo = returnData['bucket_info']
 
-    app.logger.debug(f"Updating af-details with "
-                     f"{afInfo['daily_points_remaining']} remaining points")
-    # Update the af-details doc in the DB
-    afDoc.minute_points=afInfo['minute_points']
-    afDoc.minute_points_remaining=afInfo['minute_points_remaining']
-    afDoc.daily_points=afInfo['daily_points']
-    afDoc.daily_points_remaining=afInfo['daily_points_remaining']
-    afDoc.minute_bucket_start=afInfo['minute_bucket_start']
-    afDoc.daily_bucket_start=afInfo['daily_bucket_start']
-    afDoc.save()
+    try:
+        app.logger.debug(f"Updating af-details with "
+                         f"{afInfo['daily_points_remaining']} remaining points")
+        # Update the af-details doc in the DB
+        afDoc.minute_points = afInfo['minute_points']
+        afDoc.minute_points_remaining = afInfo['minute_points_remaining']
+        afDoc.daily_points = afInfo['daily_points']
+        afDoc.daily_points_remaining = afInfo['daily_points_remaining']
+        afDoc.minute_bucket_start = afInfo['minute_bucket_start']
+        afDoc.daily_bucket_start = afInfo['daily_bucket_start']
+        afDoc.save()
+        app.logger.debug('MUEY BUENO')
+    except Exception as ex:
+        app.logger.error('NO BUENO')
+        app.logger.error(ex)
 
 
 def checkAfPoints(bucketInfo):
@@ -110,14 +116,13 @@ def getTagInfo(tagName):
 
     # Query AF and get the tag info to be stored in our local ES cache
     app.logger.debug(f'Gathering tag info for {tagName}')
-    queryResponse = requests.post(url=searchURL,headers=headers,
+    queryResponse = requests.post(url=searchURL, headers=headers,
                                   data=json.dumps(data))
     queryData = queryResponse.json()
 
     app.logger.debug(f"getTagInfo() returns: {queryData}")
 
     return queryData
-
 
 
 def processTagList(tagObj):
@@ -144,7 +149,6 @@ def processTagList(tagObj):
     return tagList
 
 
-
 def processTag(tagName):
     '''
     Method determines if we have a local tag info cache or we need to go to AF
@@ -162,7 +166,6 @@ def processTag(tagName):
                      "description": "Tag has not been assigned to a group"}]
 
     app.logger.debug(f"Querying local cache for {tagName}")
-
 
     try:
         tagDoc = TagDetailsDoc.get(id=tagName)
@@ -185,7 +188,6 @@ def processTag(tagName):
         updateDetails = True
         updateType = "Creating"
 
-
     if updateDetails:
 
         afTagData = getTagInfo(tagName)
@@ -195,19 +197,18 @@ def processTag(tagName):
         if "message" not in afTagData:
             app.logger.debug(f"{updateType} doc for {tagName}")
 
-            tagDoc = TagDetailsDoc(meta={'id': tagName},name=tagName)
+            tagDoc = TagDetailsDoc(meta={'id': tagName}, name=tagName)
             tagDoc.tag = afTagData['tag']
             tagDoc.doc_updated = now
             tagDoc.type_of_doc = "tag-doc"
             tagDoc.processed = 1
 
-                        
             # If the tag groups are empty send back Undefined
             if not afTagData['tag_groups'] or afTagData['tag_groups'] == "":
                 tagDoc.tag_groups = tagGroupDict
             else:
                 tagDoc.tag_groups = afTagData['tag_groups']
-                
+
             # Only set the doc_created attribute if we aren't updating
             if updateType == "Creating":
                 tagDoc.doc_created = now
@@ -225,11 +226,10 @@ def processTag(tagName):
                      f"{tagDoc.tag['tag_name'],tagDoc.tag['public_tag_name']}" +
                      f"{tagDoc.tag['tag_class'],tagDoc.tag_groups[0]['tag_group_name']}," +
                      f"{tagDoc.tag['description']}")
-    
-    return (tagDoc.tag['tag_name'],tagDoc.tag['public_tag_name'],
-            tagDoc.tag['tag_class'],tagDoc.tag_groups[0]['tag_group_name'],
-            tagDoc.tag['description'])
 
+    return (tagDoc.tag['tag_name'], tagDoc.tag['public_tag_name'],
+            tagDoc.tag['tag_class'], tagDoc.tag_groups[0]['tag_group_name'],
+            tagDoc.tag['description'])
 
 
 def assessTags(tagsObj):
@@ -251,7 +251,6 @@ def assessTags(tagsObj):
     #    Set the tagInfo to this but keep going in case we find a campaign
     #      or an actor
 
-
     for entry in tagsObj:
         while not taggedEvent:
             sampleDate = entry[0]
@@ -265,22 +264,22 @@ def assessTags(tagsObj):
                 app.logger.debug(f"Working on tag {tagName} " +
                                  f"with class of {tagClass}")
 
-                #import pdb;pdb.set_trace()
+                # import pdb;pdb.set_trace()
                 if tagClass == "campaign":
-                    tagInfo = {"tag_name":tagName,"public_tag_name":tag[0],
-                               "tag_class":tagClass,"sample_date":sampleDate,
-                               "file_type":sampleFileType,"tag_group":tagGroup,
-                               "description":tagDesc,"confidence_level":90}
+                    tagInfo = {"tag_name": tagName, "public_tag_name": tag[0],
+                               "tag_class": tagClass, "sample_date": sampleDate,
+                               "file_type": sampleFileType, "tag_group": tagGroup,
+                               "description": tagDesc, "confidence_level": 90}
                     taggedEvent = True
                     app.logger.debug(f"Tag info for {tagName}: {tagInfo}")
-                    break   # This the grand daddy of all tags
-                            # No need to keep processing the rest
+                    break  # This the grand daddy of all tags
+                    # No need to keep processing the rest
 
                 elif tagClass == "actor":
-                    tagInfo = {"tag_name":tagName,"public_tag_name":tag[0],
-                               "tag_class":tagClass,"sample_date":sampleDate,
-                               "file_type":sampleFileType,"tag_group":tagGroup,
-                               "description":tagDesc,"confidence_level":90}
+                    tagInfo = {"tag_name": tagName, "public_tag_name": tag[0],
+                               "tag_class": tagClass, "sample_date": sampleDate,
+                               "file_type": sampleFileType, "tag_group": tagGroup,
+                               "description": tagDesc, "confidence_level": 90}
                     taggedEvent = True
                     app.logger.debug(f"Tag info for {tagName}: {tagInfo}")
 
@@ -298,42 +297,41 @@ def assessTags(tagsObj):
                         if dateDiff.days < int(days):
                             confLevel = tagConfLevels[days]
                             app.logger.debug(f"confidence_level for " +
-                                        f"{tagName} @ date " +
-                                        f"{sampleDate}: "+
-                                        f"{confLevel} based on" +
-                                        f" age of {dateDiff.days} days")
-                            break # We found the right confidence level
+                                             f"{tagName} @ date " +
+                                             f"{sampleDate}: " +
+                                             f"{confLevel} based on" +
+                                             f" age of {dateDiff.days} days")
+                            break  # We found the right confidence level
                         else:
                             confLevel = 5
                             app.logger.debug(f"confidence_level for " +
-                                            f"{tagName} @ date " +
-                                            f"{sampleDate}: "+
-                                            f"{confLevel} based on" +
-                                            f" age of {dateDiff.days} days")
+                                             f"{tagName} @ date " +
+                                             f"{sampleDate}: " +
+                                             f"{confLevel} based on" +
+                                             f" age of {dateDiff.days} days")
 
-                    tagInfo = {"tag_name":tagName,"public_tag_name":tag[0],
-                                "tag_class":tagClass,"sample_date":sampleDate,
-                                "file_type":sampleFileType,"tag_group":tagGroup,
-                               "description":tagDesc,"confidence_level":confLevel}
+                    tagInfo = {"tag_name": tagName, "public_tag_name": tag[0],
+                               "tag_class": tagClass, "sample_date": sampleDate,
+                               "file_type": sampleFileType, "tag_group": tagGroup,
+                               "description": tagDesc, "confidence_level": confLevel}
                     taggedEvent = True
                     app.logger.debug(f"Tag info for {tagName}: {tagInfo}")
 
             # Went through all the tags available and none were of interest
             if not taggedEvent:
-                tagInfo = {"tag_name":"Low Priority Tags",
-                            "public_tag_name":"Low Priority Tags",
-                            "tag_class":"Low Priority Tags",
-                            "sample_date":"2000-01-01T00:00:00",
-                            "file_type":"Low Priority Tags",
-                            "tag_group":"Low Priority Tags",
-                            "description":"Low Priority Tags",
-                            "confidence_level":0}
+                tagInfo = {"tag_name": "Low Priority Tags",
+                           "public_tag_name": "Low Priority Tags",
+                           "tag_class": "Low Priority Tags",
+                           "sample_date": "2000-01-01T00:00:00",
+                           "file_type": "Low Priority Tags",
+                           "tag_group": "Low Priority Tags",
+                           "description": "Low Priority Tags",
+                           "confidence_level": 0}
                 taggedEvent = True
 
     app.logger.debug(f"assessTags() returns: {tagInfo}")
 
     return tagInfo
-
 
 
 def getDomainInfo(threatDomain):
@@ -344,13 +342,13 @@ def getDomainInfo(threatDomain):
     either of those values are triggered
     '''
     domainObj = list()
-    domainObj.append(('2000-01-01T00:00:00','NA',
+    domainObj.append(('2000-01-01T00:00:00', 'NA',
                       [('No Samples Returned for Domain',
                         'No Samples Returned for Domain',
                         'No Samples Returned for Domain',
                         'No Samples Returned for Domain',
                         'No Samples Returned for Domain')]))
-    apiKey= app.config['AUTOFOCUS_API_KEY']
+    apiKey = app.config['AUTOFOCUS_API_KEY']
     searchURL = app.config["AUTOFOCUS_SEARCH_URL"]
     resultURL = app.config["AUTOFOCUS_RESULTS_URL"]
     lookupTimeout = app.config["AF_LOOKUP_TIMEOUT"]
@@ -358,23 +356,23 @@ def getDomainInfo(threatDomain):
     resultData = {"apiKey": apiKey}
     headers = {"Content-Type": "application/json"}
     searchData = {"apiKey": apiKey,
-                    "query": {
-                        "operator": "all",
-                        "children": [{
-                            "field":"alias.domain",
-                            "operator":"contains",
-                            "value": threatDomain}]},
-                    "size": 100,
-                    "from": 0,
-                    "sort": {"create_date": {"order": "desc"}},
-                    "scope": "global",
-                    "artifactSource": "af"}
+                  "query": {
+                      "operator": "all",
+                      "children": [{
+                          "field": "alias.domain",
+                          "operator": "contains",
+                          "value": threatDomain}]},
+                  "size": 100,
+                  "from": 0,
+                  "sort": {"create_date": {"order": "desc"}},
+                  "scope": "global",
+                  "artifactSource": "af"}
 
     # Query AF and it returns a "cookie" that we use to view the resutls of the
     # search
 
     app.logger.debug(f'Gathering domain info for {threatDomain}')
-    queryResponse = requests.post(url=searchURL,headers=headers,
+    queryResponse = requests.post(url=searchURL, headers=headers,
                                   data=json.dumps(searchData))
     app.logger.debug(f"Initial AF domain query returned {queryResponse.json()}")
     queryData = queryResponse.json()
@@ -389,10 +387,10 @@ def getDomainInfo(threatDomain):
             # The checkAfPoints will eventually return after the points reset.
             # When they do, reurn the AF query so we don't lose it.
             app.logger.debug(f'Gathering domain info for {threatDomain}')
-            queryResponse = requests.post(url=searchURL,headers=headers,
-                                    data=json.dumps(searchData))
+            queryResponse = requests.post(url=searchURL, headers=headers,
+                                          data=json.dumps(searchData))
             app.logger.debug(f"Initial AF domain query returned "
-                            f"{queryResponse.json()}")
+                             f"{queryResponse.json()}")
             queryData = queryResponse.json()
         else:
             app.logger.error(f"Return from AutoFocus is in error: {queryData}")
@@ -409,10 +407,10 @@ def getDomainInfo(threatDomain):
         # is only valid for 2 minutes after it completes.
         for timer in range(lookupTimeout):
             time.sleep(61)
-            cookieResults = requests.post(url=cookieURL,headers=headers,
-                                        data=json.dumps(resultData))
+            cookieResults = requests.post(url=cookieURL, headers=headers,
+                                          data=json.dumps(resultData))
             domainData = cookieResults.json()
-            if domainData['af_complete_percentage'] >= maxPercentage :
+            if domainData['af_complete_percentage'] >= maxPercentage:
                 break
             else:
                 app.logger.info(f"Search completion " +
@@ -420,18 +418,18 @@ def getDomainInfo(threatDomain):
                                 f"{threatDomain} at {timer+1} minute(s): " +
                                 f"{domainData}")
 
-        if domainData['total'] !=0:
+        if domainData['total'] != 0:
             for hits in domainData['hits']:
                 tagList = processTagList(hits)
                 # reset domainObj to empty then add the returned tagList
                 domainObj = list()
                 domainObj.append((hits['_source']['finish_date'],
-                                hits['_source']['filetype'],
-                                tagList))
+                                  hits['_source']['filetype'],
+                                  tagList))
 
         else:
             app.logger.info(f"No samples found for {threatDomain} in time "
-                             f"allotted")
+                            f"allotted")
 
 
     else:
@@ -443,7 +441,6 @@ def getDomainInfo(threatDomain):
     return domainObj
 
 
-
 def getDomainDoc(domainName):
     '''
     Method to get the local domain doc info and return it to the event
@@ -453,10 +450,9 @@ def getDomainDoc(domainName):
     updateDetails = False
     now = datetime.datetime.now().replace(microsecond=0).isoformat(' ')
     timeLimit = (datetime.datetime.now() -
-                    datetime.timedelta(days=app.config['DNS_DOMAIN_INFO_MAX_AGE']))
+                 datetime.timedelta(days=app.config['DNS_DOMAIN_INFO_MAX_AGE']))
 
     app.logger.debug(f"Querying local cache for {domainName}")
-
 
     try:
         domainDoc = DomainDetailsDoc.get(id=domainName)
@@ -479,13 +475,12 @@ def getDomainDoc(domainName):
         updateDetails = True
         updateType = "Creating"
 
-
     # Either we don't have it or we determined that it's too old
     if updateDetails:
         app.logger.info(f"{updateType} domain doc for domain {domainName}")
         try:
             afDomainData = getDomainInfo(domainName)
-            domainDoc = DomainDetailsDoc(meta={'id': domainName},name=domainName)
+            domainDoc = DomainDetailsDoc(meta={'id': domainName}, name=domainName)
             domainDoc.tags = afDomainData
             domainDoc.doc_updated = now
             # Don't mess with the doc_created field if we are updating
