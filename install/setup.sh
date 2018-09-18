83fbec2684d3122f7583bb1e9875051b3dc596c6
Edward Arcuri <earcuri@paloaltonetworks.com>
2018-09-17 18:00:02 -0600

    fixing indexes to allow for customer info

--- install/setup.sh
+++ install/setup.sh
@@ -2,16 +2,51 @@
 # Push the ElasticSearch index mappings into the ElasticSearch DB
 # NOTE: If you are using something other than the default install, you may need
 # change the elasticsearch settings below to your IP address
-echo "Installing index mappings into ElasticSearch"
+printf "$(tput setaf 6)Installing index mappings into ElasticSearch$(tput sgr 0)\n"
+printf "\n$(tput setaf 6)Installing af-details mapping$(tput sgr 0)\n"
 curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/af-details/' -d @../infra/elasticsearch/mappings/af-details.json
-curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/_template/traffic?pretty' -d @../infra/elasticsearch/mappings/traffic_template_mapping.json
+printf "\n\n$(tput setaf 6)Installing threat mapping$(tput sgr 0)\n"
 curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/_template/threat?pretty' -d @../infra/elasticsearch/mappings/threat_template_mapping.json
+printf "\n$(tput setaf 6)Installing domain detail mapping$(tput sgr 0)\n"
 curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/sfn-domain-details/' -d @../infra/elasticsearch/mappings/sfn-domain-details.json
+printf "\n\n$(tput setaf 6)Installing tag mapping$(tput sgr 0)\n"
 curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/sfn-tag-details/' -d @../infra/elasticsearch/mappings/sfn-tag-details.json
+printf "\n\n$(tput setaf 6)Updating number of replicas to 0$(tput sgr 0)\n"
 curl -XPUT -H'Content-Type: application/json' 'elasticsearch:9200/_settings' -d '{"index" : {"number_of_replicas" : 0}}'
+# The traffic mappings are not installed by default - but here is the command if you want it
+# curl -XPUT -H'Content-Type: application/json' 'http://localhost:9200/_template/traffic?pretty' -d @../infra/elasticsearch/mappings/traffic_template_mapping.json
 
-echo "\n"
+# Set up the Elasticsearch heap size
+HEAP_SIZE=8g
+EXPORT_LINE="export ES_HEAP_SIZE=${HEAP_SIZE}"
+printf "\n\n$(tput setaf 6)Setting ElasticSearch heap size to 8GB in environment$(tput sgr 0)\n"
+# Check to see if the env var is already set in the .bashrc, if it is just 
+# force it in the .bashrc to our setting here, if not, add it.  
+grep ES_HEAP_SIZE ~/.bashrc
+if [ $? == 0 ]; then
+    unset ES_HEAP_SIZE
+    echo "Found pre-set heap size, changing to ${HEAP_SIZE}"
+    sed -i -E "s/(.+ES_HEAP_SIZE.+)/${EXPORT_LINE}/g" ~/.bashrc
+else
+    echo "export ES_HEAP_SIZE=${HEAP_SIZE}" >>~/.bashrc
+fi    
 
+# Source the .bashrc so the heap size setting is in the env
+echo "Reading environment"
+source ~/.bashrc
+grep "export ES_HEAP_SIZE=${HEAP_SIZE}" ~/.bashrc
+if [ $? == 0 ]; then
+    echo "$(tput setaf 2)$(tput bold)The ElasticSearch heap size has been set in the .bashrc file"
+    echo "For the environment to have the appropriate settings, run 'source ~/.bashrc'$(tput sgr 0)"
+else
+    echo "$(tput set af1)$(tput bold)You may need to edit the .bashrc and manually put" 
+    echo "'export ES_HEAP_SIZE=${HEAP_SIZE}' in it to set the heap size.$(tput sgr 0)"
+fi
+################################################################################
+# THE FOLLOWING IS DEPRECATED AND THE SFN APPLICATION WILL NO LONGER BE RUN AS
+# A SERVICE IN FUTURE RELEASES.  IT IS LEFT HERE IN CASE YOU WANT TO TRY AND 
+# RUN IT AS A SERVICE, BUT IT IS NOT SUPPORTED
+################################################################################
 # This sets up the automatic startup of SFN if the system reboots.  It also gives
 # the ability to control SafeNetworking as a service using the systemctl and
 # service commands
