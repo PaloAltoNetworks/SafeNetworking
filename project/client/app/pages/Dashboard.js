b0e3a925863b708a810bfeb1d1921f201db544a0
Edward Arcuri <earcuri@paloaltonetworks.com>
2018-10-25 10:22:31 -0600

    remove project/client directory

--- project/client/app/pages/Dashboard.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class Dashboard extends Component {
-  render() {
-    return <iframe src="http://pe:5601/app/kibana#/dashboard/494c4f70-ff94-11e7-b08d-4f64676a2f8a?embed=true&_g=(refreshInterval%3A(display%3AOff%2Cpause%3A!f%2Cvalue%3A0)%2Ctime%3A(from%3Anow-1y%2Cmode%3Aquick%2Cto%3Anow))" height="600" width="800"></iframe>;
-  }
-}
