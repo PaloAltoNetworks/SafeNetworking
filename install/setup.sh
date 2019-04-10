8fb9e8aa1baea61401eb6ad1281a5dda307cde61
Edward Arcuri <edward.arcuri@gmail.com>
2019-04-09 21:38:39 -0600

    Remove TCP option from filters in logstash
    Rremoved input filters for TCP in all logstash .conf files for pipelines
    Also removed non-used customer.conf file

--- install/setup.sh
+++ install/setup.sh
@@ -18,20 +18,25 @@ done
 # Figure out who we are so we write the correct paths
 userName=$(echo $SUDO_USER)
 userHome=$(eval echo "~$userName" )
-printf "\n>>> $(tput setaf 3)Setting up for user $userName in $userHome directory $(tput sgr 0)\n"
+printf "\n>>> $(tput setaf 6)Setting up for user $(tput sgr 0)$userName \
+$(tput setaf 6)in $(tput sgr 0)$userHome $(tput setaf 6)directory $(tput sgr 0)\n"
 
 # Create backup directory and make world writeable so elasticsearch can use it.
 if [ ! -d "$userHome/es_backup" ]; then
+  printf "\n>>> $(tput setaf 2)Creating directory for ES backups$(tput sgr 0)\n"
   install -d -m 0777 -o $userName -g $(id -gn $userName) $userHome/es_backup
+else
+  printf "\n>>> $(tput setaf 1)Backup directory already exists, skipping$(tput sgr 0)\n"
 fi
 
-#if [ ! -d "$userHome/safe-networking/env" ]; then
-#  cd $userHome/safe-networking
-#  python3.6 -m venv env
-#  source env/bin/activate
-#  pip install --upgrade pip
-#  pip install -r requirements
-#fi
+if [ ! -d "$userHome/safe-networking/.env" ]; then
+ cd $userHome/safe-networking
+ python3.6 -m venv .env
+ source .env/bin/activate
+ pip install --upgrade pip
+ pip install -r requirements
+fi
+
 
 ################################################################################
 #                       ELASTICSTACK SETUP
@@ -39,7 +44,7 @@ fi
 #                       ELASTICSEARCH SETUP
 #
 # Copy over the config files that are needed for SFN to work in a PoC env
-# First thing we do is 
+#  
 printf "\n>>> $(tput setaf 6)Backing up elasticsearch config files$(tput sgr 0)"
 find ./elasticsearch/config/elasticsearch_template.yml -exec sed "s#_USER_HOME_#$userHome#g" {} \; > ./elasticsearch/config/elasticsearch.yml 
 cp /etc/elasticsearch/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml.$(date +%F_%R)
@@ -49,6 +54,43 @@ printf ">>> $(tput setaf 6)Installing new elasticsearch config files$(tput sgr 0
 cp ./elasticsearch/config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
 cp ./elasticsearch/config/jvm.options /etc/elasticsearch/jvm.options
 printf " - COMPLETE\n"
+printf ">>> $(tput setaf 6)Configuring limits.conf and sysctl.conf settings$(tput sgr 0)"
+
+if [ $( grep -ic "elasticsearch  -  memlock unlimited" /etc/security/limits.conf ) -ne 1 ]
+then 
+    printf "\n\t>>> $(tput setaf 2)Adding memorylock$(tput sgr 0)"
+    sed -i '/End of file/i elasticsearch  -  memlock unlimited' /etc/security/limits.conf
+else
+    printf "\n\t>>> $(tput setaf 1)Memory lock already set in limits.conf, skipping$(tput sgr 0)"
+fi
+
+if [ $( grep -ic "elasticsearch  -  nofile 65535" /etc/security/limits.conf ) -ne 1 ]
+then 
+    printf "\n\t>>> $(tput setaf 2)Adding number of open files$(tput sgr 0)"
+    sed -i '/End of file/i elasticsearch  -  nofile 65535' /etc/security/limits.conf
+else
+    printf "\n\t>>> $(tput setaf 1)No. of files already set in limits.conf, skipping$(tput sgr 0)"
+fi
+
+if [ $( grep -ic "elasticsearch  -  noproc 4096" /etc/security/limits.conf ) -ne 1 ]
+then 
+    printf "\n\t>>> $(tput setaf 2)Adding number of processes$(tput sgr 0)"
+    sed -i '/End of file/i elasticsearch  -  noproc 4096' /etc/security/limits.conf
+else
+    printf "\n\t>>> $(tput setaf 1)Number of processes already set in limits.conf, skipping$(tput sgr 0)"    
+fi
+
+if [ $( grep -ic "vm.max_map_count=262144" /etc/sysctl.conf ) -ne 1 ]
+then 
+    printf "\n\t>>> $(tput setaf 2)Setting VM mmap count$(tput sgr 0)"
+    sed -i '/End of File/i elasticsearch  -  noproc 4096' /etc/security/limits.conf
+else
+    printf "\n\t>>> $(tput setaf 1)VM mmap count already set in sysctl.conf, skipping$(tput sgr 0)\n"    
+fi
+
+
+################################################################################
+#                            LOGSTASH SETUP
 printf ">>> $(tput setaf 6)Installing logstash pipelines and config files$(tput sgr 0)"
 if [ ! -d "/etc/logstash/pipelines" ]; then
     install -d -m 0777 -o $userName -g $(id -gn $userName) /etc/logstash/pipelines 
@@ -57,7 +99,11 @@ cp ./logstash/*.conf /etc/logstash/pipelines/
 cp /etc/logstash/pipelines.yml /etc/logstash/pipelines.yml.$(date +%F_%R)
 cp ./logstash/pipelines.yml /etc/logstash/pipelines.yml
 cp /etc/logstash/logstash.yml /etc/logstash/logstash.yml.$(date +%F_%R)
-cp ./logstash/logstash.yml /etc/logstash/logstash.yml
+cp ./logstash/config/logstash.yml /etc/logstash/logstash.yml
+cp /etc/logstash/jvm.options /etc/logstash/jvm.options.$(date +%F_%R)
+cp ./logstash/config/jvm.options /etc/logstash/jvm.options
+cp /etc/logstash/startup.options /etc/logstash/startup.options.$(date +%F_%R)
+cp ./logstash/config/startup.options /etc/logstash/startup.options
 printf " - COMPLETE\n"
 printf "\n>>> $(tput setaf 6)Backing up kibana config files$(tput sgr 0)"
 cp /etc/kibana/kibana.yml /etc/kibana/kibana.yml.$(date +%F_%R)
@@ -90,6 +136,11 @@ else
         exit 7
     fi
 fi
+# Test the limits set above
+curl -X GET "localhost:9200/_nodes/stats/process?filter_path=**.max_file_descriptors"
+curl -X GET "localhost:9200/_nodes?filter_path=**.mlockall"
+
+
 
 printf "  >>> $(tput setaf 6)Setting up logstash auto-start$(tput sgr 0)\n"
 /bin/systemctl daemon-reload
@@ -163,44 +214,58 @@ curl -XPUT -H'Content-Type: application/json' 'localhost:9200/_settings' \
 
 # Load the GTP and IoT databases for enrichment
 if [ installGTP ]
-    printf "\n\n$(tput setaf 6)Installing GTP Event Code Documents$(tput sgr 0)\n"
     then
+        printf "\n\n$(tput setaf 6)Installing GTP Event Code Documents$(tput sgr 0)\n"
         for file in `ls $userHome/safe-networking/install/elasticsearch/lookup_data/gtp/*.csv`
             do
                 ./sfn load $file test-gtp-codes
             done
 fi
 
+################################################################################
+#                           SFN SETUP
+#
+# check to see if we need to install the .panrc in the home directory, otherwise
+# just link it
+if [ ! -f $userHome/.panrc ]
+    then
+        printf "\n\n$(tput setaf 6)Installing .panrc file to home directory$(tput sgr 0)\n"
+        cp sfn/.panrc $userHome
+fi
+printf "\n\n$(tput setaf 6)Linking .panrc to project$(tput sgr 0)\n"
+cd $userHome/safe-networking/project
+ln -s ~/.panrc
+printf " - COMPLETE\n"
 
-################################################################################
-# THE FOLLOWING IS DEPRECATED AND THE SFN APPLICATION WILL NO LONGER BE RUN AS
-# A SERVICE IN FUTURE RELEASES.  IT IS LEFT HERE IN CASE YOU WANT TO TRY AND 
-# RUN IT AS A SERVICE, BUT IT IS NOT SUPPORTED
-################################################################################
-# This sets up the automatic startup of SFN if the system reboots.  It also gives
-# the ability to control SafeNetworking as a service using the systemctl and
-# service commands
-# INSTALL_DIR="$(dirname `pwd`)"
-# START_FILE=$INSTALL_DIR/install/sfn/sfn.sh
-# echo "Creating SafeNetworking startup file $START_FILE"
-# echo "...."
-# echo "#!/bin/sh -" >$START_FILE
-# echo "# SafeNetworking startup file" >>$START_FILE
-# echo "cd $INSTALL_DIR" >>$START_FILE
-# echo "$INSTALL_DIR/env/bin/python $INSTALL_DIR/sfn" >>$START_FILE
-# echo ""
-# echo "Moving startup file to /usr/local/bin -->"
-# echo "...."
-# sudo cp $START_FILE /usr/local/bin/sfn.sh
-# sudo chmod 755 /usr/local/bin/sfn.sh
-# echo ""
-# echo "Copying sfn.service to /etc/systemd/system -->"
-# echo "...."
-# sudo cp $INSTALL_DIR/install/sfn/sfn.service /etc/systemd/system/sfn.service
-# echo ""
-# sudo chmod 644 /etc/systemd/system/sfn.service
-# echo "Enabling and starting the SafeNetworking Service - this may take a minute"
-# sudo systemctl daemon-reload
-# sudo systemctl enable sfn.service
-# sudo systemctl start sfn.service
-# sudo systemctl status sfn.service
+# ################################################################################
+# # THE FOLLOWING IS DEPRECATED AND THE SFN APPLICATION WILL NO LONGER BE RUN AS
+# # A SERVICE IN FUTURE RELEASES.  IT IS LEFT HERE IN CASE YOU WANT TO TRY AND 
+# # RUN IT AS A SERVICE, BUT IT IS NOT SUPPORTED
+# ################################################################################
+# # This sets up the automatic startup of SFN if the system reboots.  It also gives
+# # the ability to control SafeNetworking as a service using the systemctl and
+# # service commands
+# # INSTALL_DIR="$(dirname `pwd`)"
+# # START_FILE=$INSTALL_DIR/install/sfn/sfn.sh
+# # echo "Creating SafeNetworking startup file $START_FILE"
+# # echo "...."
+# # echo "#!/bin/sh -" >$START_FILE
+# # echo "# SafeNetworking startup file" >>$START_FILE
+# # echo "cd $INSTALL_DIR" >>$START_FILE
+# # echo "$INSTALL_DIR/env/bin/python $INSTALL_DIR/sfn" >>$START_FILE
+# # echo ""
+# # echo "Moving startup file to /usr/local/bin -->"
+# # echo "...."
+# # sudo cp $START_FILE /usr/local/bin/sfn.sh
+# # sudo chmod 755 /usr/local/bin/sfn.sh
+# # echo ""
+# # echo "Copying sfn.service to /etc/systemd/system -->"
+# # echo "...."
+# # sudo cp $INSTALL_DIR/install/sfn/sfn.service /etc/systemd/system/sfn.service
+# # echo ""
+# # sudo chmod 644 /etc/systemd/system/sfn.service
+# # echo "Enabling and starting the SafeNetworking Service - this may take a minute"
+# # sudo systemctl daemon-reload
+# # sudo systemctl enable sfn.service
+# # sudo systemctl start sfn.service
+# # sudo systemctl status sfn.service
